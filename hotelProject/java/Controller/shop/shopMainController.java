1cd2002c7e21bde1b16d35665b1e138d4d5f242d
HKEDU <HKEDU@DESKTOP-RFU25K9>
2020-04-09 12:55:10 +0900

    04/09 신지우

--- hotelProject/java/Controller/shop/shopMainController.java
+++ hotelProject/java/Controller/shop/shopMainController.java
@@ -1,3 +1,4 @@
+
 package Controller.shop;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +9,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import service.shop.goodsListService;
 
 @Controller
-public class shopListController {
+public class shopMainController {
 	@Autowired
 	goodsListService shopListService;
-	@RequestMapping("/delshop/categories")
-	public String goodsList(Model model) {
+	
+	@RequestMapping("delshop/main")
+	public String shopMain(Model model) {
 		shopListService.goodsList(model);
-		return "shop/categories";
+		return "shop/shopIndex";
 	}
+
+	@RequestMapping("/delshop/sa")
+	public String goodsInsert() {
+		return "shop/goodsInsert";
+	}
+	
 	
 }
